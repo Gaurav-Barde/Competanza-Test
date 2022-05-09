0175950e2e30217e3b555404f33db38f6a974fb2
Gaurav-Barde <gabarde25@gmail.com>
2022-05-09 12:13:05 +0530

    Added SignUp Screen in Stack.Screen

--- RootNavigation.js
+++ RootNavigation.js
@@ -3,11 +3,10 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {useSelector} from 'react-redux';
 
-
-
 //Components
 import LoginScreen from './src/Screens/LoginScreen';
 import TabNavigation from './TabNavigation';
+import SignUpScreen from './src/Screens/SignUpScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -26,6 +25,7 @@ const RootNavigation = () => {
         ) : (
           <Stack.Screen name="LoginScreen" component={LoginScreen} />
         )}
+        <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
