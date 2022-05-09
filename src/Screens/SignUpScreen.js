5c2729d6dd742d13fca52e6191a96a01e8c63987
Gaurav-Barde <gabarde25@gmail.com>
2022-05-09 13:30:14 +0530

    Added Alert Messages for errors

--- src/Screens/SignUpScreen.js
+++ src/Screens/SignUpScreen.js
@@ -19,10 +19,12 @@ const SignUpScreen = ({navigation}) => {
       .catch(error => {
         if (error.code === 'auth/email-already-in-use') {
           console.log('That email address is already in use!');
+          alert('That email address is already in use!');
         }
 
         if (error.code === 'auth/invalid-email') {
           console.log('That email address is invalid!');
+          alert('That email address is invalid!');
         }
 
         console.error(error);
